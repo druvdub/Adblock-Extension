0432ca8076a127baa9f5f56d34d630e05c838229
ddos <59387969+druvdub@users.noreply.github.com>
2022-07-02 03:07:30 +0530

    Adblock v1

---
+++ background.js
@@ -0,0 +1,29 @@
+const blockFilter = [
+  "*://*.doubleclick.net/*",
+  "*://partner.googleadservices.com/*",
+  "*://*.googlesyndication.com/*",
+  "*://*.google-analytics.com/*",
+  "*://creative.ak.fbcdn.net/*",
+  "*://*.adbrite.com/*",
+  "*://*.exponential.com/*",
+  "*://*.quantserve.com/*",
+  "*://*.scorecardresearch.com/*",
+  "*://*.zedo.com/*",
+  "*://*.soundtrap.com/*",
+  "*://spotifycapture.page.link/*",
+  "",
+];
+
+var callback = function (details) {
+  return { cancel: true };
+};
+
+var filter = { urls: blockFilter };
+
+var opt_extraInfoSpec = ["blocking"];
+
+chrome.webRequest.onBeforeRequest.addListener(
+  callback,
+  filter,
+  opt_extraInfoSpec
+);
